09c378047fa9775d7bc4abc7aae918d8c12d0d67
tomaszociepa <tom.ociepa@gmail.com>
2019-06-19 14:51:02 +0200

    exercise 6

---
+++ src/com/exercises/FunctionTest.java
@@ -0,0 +1,29 @@
+package com.exercises;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class FunctionTest {
+
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("Przem", "Tomasz", "Klaudia");
+
+        System.out.println(list);
+
+
+        Function<List<String>, List<Integer>> f = inputList -> {
+          List<Integer> result = new ArrayList<>();
+         inputList.forEach(o -> result.add(o.length()));
+         return result;
+        };
+
+        List<Integer> integerList = f.apply(list);
+
+        System.out.println(integerList);
+
+
+    }
+}

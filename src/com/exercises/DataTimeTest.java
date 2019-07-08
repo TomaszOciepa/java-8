9f8103f582604fd27cd5fc250c3e7ce31b380778
tomaszociepa <tom.ociepa@gmail.com>
2019-07-08 15:43:04 +0200

    exercise 24

--- src/com/exercises/DataTimeTest.java
+++ src/com/exercises/DataTimeTest.java
@@ -8,65 +8,73 @@ import java.util.Random;
 public class DataTimeTest {
     public static void main(String[] args) {
 
-//        LocalTime localTime = LocalTime.of(7, 15);
-//        System.out.println(localTime);
-//        System.out.println(localTime.getHour());
-//        System.out.println(localTime.getMinute());
-//        System.out.println(localTime.getSecond());
-//
-//        System.out.println(localTime.toString());
-//
-//        LocalTime lt2 = LocalTime.parse("16:10:41.123");
-//        System.out.println(lt2);
-//        System.out.println(lt2.getNano());
-//        System.out.println(lt2.get(ChronoField.MILLI_OF_SECOND));
+        //        LocalTime localTime = LocalTime.of(7, 15);
+        //        System.out.println(localTime);
+        //        System.out.println(localTime.getHour());
+        //        System.out.println(localTime.getMinute());
+        //        System.out.println(localTime.getSecond());
+        //
+        //        System.out.println(localTime.toString());
+        //
+        //        LocalTime lt2 = LocalTime.parse("16:10:41.123");
+        //        System.out.println(lt2);
+        //        System.out.println(lt2.getNano());
+        //        System.out.println(lt2.get(ChronoField.MILLI_OF_SECOND));
 
-//        LocalDateTime ldt = LocalDateTime.of(2019, 07, 03, 17, 35);
-//
-//        System.out.println(ldt);
-//
-//        LocalTime lt = ldt.toLocalTime();
-//        System.out.println(lt);
-//
-//        LocalDate ld = ldt.toLocalDate();
-//        System.out.println(ld);
+        //        LocalDateTime ldt = LocalDateTime.of(2019, 07, 03, 17, 35);
+        //
+        //        System.out.println(ldt);
+        //
+        //        LocalTime lt = ldt.toLocalTime();
+        //        System.out.println(lt);
+        //
+        //        LocalDate ld = ldt.toLocalDate();
+        //        System.out.println(ld);
 
-//        ZoneId zoneId = ZoneId.systemDefault();
-//
-//        System.out.println("Defoult zone "+zoneId);
-//
-//        ZoneId zoneId1 = ZoneId.of("Australia/Sydney");
-//
-//        LocalDateTime ldt = LocalDateTime.now();
-//        System.out.println(ldt);
-//
-//        ZonedDateTime zdt = ZonedDateTime.of(ldt, zoneId1);
-//
-//        System.out.println(zdt);
-//
-//        ZonedDateTime zonedDateTime = ZonedDateTime.now();
-//
-//        System.out.println(zonedDateTime);
+        //        ZoneId zoneId = ZoneId.systemDefault();
+        //
+        //        System.out.println("Defoult zone "+zoneId);
+        //
+        //        ZoneId zoneId1 = ZoneId.of("Australia/Sydney");
+        //
+        //        LocalDateTime ldt = LocalDateTime.now();
+        //        System.out.println(ldt);
+        //
+        //        ZonedDateTime zdt = ZonedDateTime.of(ldt, zoneId1);
+        //
+        //        System.out.println(zdt);
+        //
+        //        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        //
+        //        System.out.println(zonedDateTime);
 
-//        LocalTime now = LocalTime.now();
-//        System.out.println(now);
-//
-//        Duration duration = Duration.of(10, ChronoUnit.MINUTES);
-//
-//        System.out.println(now.plus(duration));
-//
-//        LocalTime lt = LocalTime.of(8, 0);
-//
-//        System.out.println(Duration.between(now, lt));
+        //        LocalTime now = LocalTime.now();
+        //        System.out.println(now);
+        //
+        //        Duration duration = Duration.of(10, ChronoUnit.MINUTES);
+        //
+        //        System.out.println(now.plus(duration));
+        //
+        //        LocalTime lt = LocalTime.of(8, 0);
+        //
+        //        System.out.println(Duration.between(now, lt));
 
-        LocalTime start = LocalTime.now();
-        for (int i = 0; i < 1000; i++) {
-            System.out.println(new Random().nextInt());
-        }
+        //        LocalTime start = LocalTime.now();
+        //        for (int i = 0; i < 1000; i++) {
+        //            System.out.println(new Random().nextInt());
+        //        }
+        //
+        //        LocalTime stop = LocalTime.now();
+        //
+        //        System.out.println(Duration.between(start, stop));
 
-        LocalTime stop = LocalTime.now();
+        LocalDateTime now = LocalDateTime.now();
 
-        System.out.println(Duration.between(start, stop));
+        LocalDateTime nowPlus2H = now.plusHours(2).plusDays(2).plusMonths(2).plusYears(2);
+
+        System.out.println(now);
+        System.out.println(nowPlus2H);
+
 
     }
 }
